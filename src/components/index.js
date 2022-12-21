99ff0c3f7b93704da76beaa006f2ea8985aafd5b
Devansh707 <devanshsrivastava707@gmail.com>
2022-12-21 16:48:18 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from './Navbar';
+import Billing from "./Billing";
+import Button from "./Button";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from './Stats';
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+import Hero from "./Hero";
+
+export{ 
+    Navbar,
+    Billing,
+    Button,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+    CardDeal,
+};
